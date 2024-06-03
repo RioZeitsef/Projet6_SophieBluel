de7c520e4780b513091b90bf1b2269465257941f
Matthieu Metz <metz.matthieu@gmail.com>
2024-06-03 06:34:26 +0200

    Tri des fonctions modification chgt aync fetch

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -1,12 +1,16 @@
-async function listWorks () {
-    const response = await fetch('http://localhost:5678/api/works');
-    return response.json();
-}
+let _works = [];
+let _categories = [];
 
-async function init() {
-    _works = await listWorks();
+function getWorks() {
+    fetch('http://localhost:5678/api/works')
+        .then(resp => resp.json())
+        .then(works => {
+            _works.push(...works);
+            initCategories(works);
+            displayWorks(works);
+        });
+}        
 
-    console.log(listWorks());
     function createButton(text, onClick) {
         const button = document.createElement('button');
         button.textContent = text;
@@ -14,41 +18,44 @@ async function init() {
         return button;
     }
 
-    function filterWorks(categoryId) {
-        return categoryId === 0 ? _works : _works.filter(work => work.category.id === categoryId);
+    function createCategorieButton(categories) {
+        const portfolioSection = document.getElementById('portfolio');
+        const h2Element = portfolioSection.querySelector('h2');
+        const buttonAll = createButton('Tous', () => displayWorks(filterWorks(0)));
+        portfolioSection.insertBefore(buttonAll, h2Element.nextSibling);
+
+        function filterWorks(categoryId) {
+            return categoryId === 0 ? _works : _works.filter(work => work.category.id === categoryId);
     }
 
-    function displayWorks(listWorks) {
-        const gallery = document.querySelector('#portfolio .gallery');
-        gallery.innerHTML = '';
-        listWorks.forEach(work => {
-            const figure = document.createElement('figure');
-            const img = document.createElement('img');
-            img.src = work.imageUrl;
-            img.alt = work.title;
-            const figcaption = document.createElement('figcaption');
-            figcaption.textContent = work.title;
-
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-            gallery.appendChild(figure);
-        });
-    }
-
-    const portfolioSection = document.getElementById('portfolio');
-    const h2Element = portfolioSection.querySelector('h2');
-    const buttonAll = createButton('Tous', () => displayWorks(filterWorks(0)));
-    portfolioSection.insertBefore(buttonAll, h2Element.nextSibling);
-
-    const uniqueCategories = Array.from(new Set(_works.map(work => work.category.id)))
-        .map(id => _works.find(work => work.category.id === id).category);
-    uniqueCategories.forEach(category => {
+    categories.forEach(category => {
         const button = createButton(category.name, () => displayWorks(filterWorks(category.id)));
         portfolioSection.insertBefore(button, h2Element.nextSibling.nextSibling);
     });
+}        
 
-displayWorks(_works);
+function initCategories(works) {
+    _categories = Array.from(new Set(works.map(work => work.category.id)))
+        .map(id => works.find(work => work.category.id === id).category);
+    createCategorieButton(_categories);
+}    
 
+
+function displayWorks(listWorks) {
+    const gallery = document.querySelector('#portfolio .gallery');
+    gallery.innerHTML = '';
+    listWorks.forEach(work => {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
 }
 
-init();
+getWorks();
