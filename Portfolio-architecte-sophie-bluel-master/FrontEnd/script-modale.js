37b67905815a56dfee4ae19957bb4879558e9b7e
Matthieu Metz <metz.matthieu@gmail.com>
2024-06-20 18:35:16 +0200

    mise à jour modal

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script-modale.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script-modale.js
@@ -37,4 +37,18 @@ function displayWorks(listWorks, categoryId) {
 
 getWorks();
 
+const openModal = function (e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal) 
+    
+    });
+
+
 
