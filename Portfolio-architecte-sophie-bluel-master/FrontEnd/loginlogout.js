f1eea65147e904c41f67683b50e6319930c26550
Matthieu Metz <metz.matthieu@gmail.com>
2024-09-05 12:28:02 +0200

    ajout de la bannière  "Mode édition" fonctionnelle avec la condition du token

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/loginlogout.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/loginlogout.js
@@ -3,6 +3,13 @@ function updateLoginStatus () {
     if (token) {
         document.getElementById('login').style.display = 'none';
         document.getElementById('logout').style.display = 'block';
+        var removeDisplay = document.getElementById("edition-mode");
+        if (removeDisplay) {
+            removeDisplay.style.display = removeDisplay.style.display === 'none' ? '' : 'none';
+        }
+        
+        var headerLogin = document.querySelector('header');
+        headerLogin.classList.add('header-login');
     } else {
         document.getElementById('login').style.display = 'block';
         document.getElementById('logout').style.display = 'none';
